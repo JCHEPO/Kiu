a79eae702136ce6599e4e38a8bcd05106cecaa1b
Juan Chepo <juanmanuelcheporivas@gmail.com>
2025-09-22 23:21:01 -0300

    combinando cambios

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
+<<<<<<< HEAD
 function textoElemento (elemento,texto){
     let titulo = documents.querySelector();
     titulo.innerHTML = texto;
 }
 
 let listaAmigos = [];
+=======
+let listaAmigos =[];
+
+function ElementoTextoFun (elemento,texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+}
+
+>>>>>>> secundaria
 function agregarAmigo() {
-        // si el nombre está incluido, lo agregamos, si no ponemos alert.
-    if(listaAmigos.includes(document.getElementById(`amigo`).value)){
+    let nombre = document.getElementById('amigo').value.trim();
+
+    if (nombre === ""){   // verificar que la casilla esté vacia
+        ElementoTextoFun('p', 'Ingresa un nombre');
+        return
+    }
+        
+    if(listaAmigos.includes(nombre)){ // alert si el nombre ya está en la lista
         alert("ese nombre ya existe");
-    } else {
-        listaAmigos.push(document.getElementById(`amigo`).value);
+    } else {        // si el nombre no está incluido, lo agregamos
+        listaAmigos.push(nombre);
         console.log(listaAmigos);
         limpiarCampo();
-        return;
-    
-    }
+        ElementoTextoFun('p', `tienes a ${listaAmigos}`);
+        }
     return;
 }
 
 function limpiarCampo(){
     let valorCampo = document.querySelector('#amigo').value="";
+<<<<<<< HEAD
+=======
+}
+
+
+function sortearAmigo(){
+    if(listaAmigos.length===0) {
+        alert("Tu lista de amigos está vacia");
+    } else {
+        let indice=Math.floor(Math.random()*(listaAmigos.length-1))+1; // generar un indice dentro del array
+        console.log(indice); 
+        console.log(listaAmigos[indice]);
+        ElementoTextoFun('h2', `Tu amigo secreto es ${listaAmigos[indice]}`);
+    }
+>>>>>>> secundaria
 }
 
